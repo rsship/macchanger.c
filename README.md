767951f6994cee6664cc73b26fb8cbdcc83b1162
bozkayasalihx <jamesgosling10@gmail.com>
2024-02-17 22:04:17 +0300

    add cross platform to-do

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 all modern chips, not the old ones, does supports the mac address randomization, 
 but some of old wifi drivers does not supports it. so this lib will help you with this problem 
+### TODO
+- [x] Linux
+- [ ] MacOS
+- [ ] Win32
 
 ```console
 make
--- macwatcher.c
+++ macwatcher.c
@@ -13,7 +13,22 @@
 #include <time.h>
 #include <unistd.h>
 
-unsigned char *get_mac(int sock, const char *ifname) {
+
+void usage()
+{
+  printf("MAC WATCHER\n");
+  printf("    Usage macwatcher <INTERFACE_NAME>\n");
+}
+
+char *shift_args(int *argc, char ***argv) {
+  assert(*argc > 0);
+  char *result = **argv;
+  *argc -= 1;
+  *argv += 1;
+  return result;
+}
+
+unsigned char *get_mac_linux(int sock, const char *ifname) {
   struct ifreq ifr;
   memset(&ifr, 0, sizeof(ifr));
   strcpy(ifr.ifr_name, ifname);
@@ -27,48 +42,23 @@ unsigned char *get_mac(int sock, const char *ifname) {
   return mac;
 }
 
-char *shift_args(int *argc, char ***argv) {
-  assert(*argc > 0);
-  char *result = **argv;
-  *argc -= 1;
-  *argv += 1;
-  return result;
-}
 
-int main(int argc, char **argv) {
-  const char *program = shift_args(&argc, &argv);
-  const char *interface_name = shift_args(&argc, &argv);
-  // const char *interface_name = "wlx90de80910f38";
-  printf("RUNNING %s\n", program);
-
-  srand(time(NULL));
-
+int set_mac_linux(int sockfd, const char *interface_name) {
   struct ifreq ifr;
-
-  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (sockfd < 0) {
-    printf("could not create socket\n");
-    return 0;
-  }
-
-  unsigned char *cur_mac = get_mac(sockfd, interface_name);
-  printf("CURRENT MAC ADDRESS\n");
-  printf("%02X:%02X:%02X:%02X:%02X:%02X\n", cur_mac[0], cur_mac[1], cur_mac[2],
-         cur_mac[3], cur_mac[4], cur_mac[5]);
-
   // TURN DOWN INTERFACE;
+  printf("TURNING DOWN WI-FI INTERFACE\n");
   strncpy(ifr.ifr_name, interface_name, IFNAMSIZ);
   if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
     printf("could not turn down %s\n", interface_name);
     close(sockfd);
-    return 0;
+    return 1;
   }
 
   ifr.ifr_flags &= ~IFF_UP;
   if (ioctl(sockfd, SIOCSIFFLAGS, &ifr) < 0) {
     printf("could not set flags dowl %s\n", interface_name);
     close(sockfd);
-    return 0;
+    return 1;
   }
 
   ifr.ifr_hwaddr.sa_family = ARPHRD_ETHER;
@@ -82,28 +72,68 @@ int main(int argc, char **argv) {
   if (ioctl(sockfd, SIOCSIFHWADDR, &ifr) < 0) {
     printf("could not set new mac %s\n", interface_name);
     close(sockfd);
-    return 0;
+    return 1;
   }
 
   printf("SETTED MAC ADDRESS\n");
   printf("%02X:%02X:%02X:%02X:%02X:%02X\n", new_mac[0], new_mac[1], new_mac[2],
          new_mac[3], new_mac[4], new_mac[5]);
 
-  printf("MAC ADDRESS SET DONE\n");
-
   // TURN UP INTERFACE AGAIN
-  if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
-    printf("could not back up %s\n", interface_name);
-    close(sockfd);
-    return 0;
-  }
-
+  printf("TURNING UP WI-FI INTERFACE\n");
   ifr.ifr_flags |= IFF_UP;
   if (ioctl(sockfd, SIOCSIFFLAGS, &ifr) < 0) {
     printf("could not set up flags %s\n", interface_name);
     close(sockfd);
-    return 0;
+    return 1;
   }
+
+  return 0;
+}
+
+
+int main(int argc, char **argv) {
+  if(argc < 2) {
+    usage();
+    return 1;
+  }
+
+  const char *program = shift_args(&argc, &argv);
+  const char *interface_name = shift_args(&argc, &argv);
+  // const char *interface_name = "wlx90de80910f38";
+  printf("RUNNING %s\n", program);
+
+  srand(time(NULL));
+
+  // struct ifreq ifr;
+
+  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sockfd < 0) {
+    printf("could not create socket\n");
+    return -1;
+  }
+
+#ifdef __linux__
+  unsigned char *cur_mac = get_mac_linux(sockfd, interface_name);
+  printf("CURRENT MAC ADDRESS\n");
+  printf("%02X:%02X:%02X:%02X:%02X:%02X\n", cur_mac[0], cur_mac[1], cur_mac[2],
+         cur_mac[3], cur_mac[4], cur_mac[5]);
+
+  if(set_mac_linux(sockfd, interface_name) < 0) {
+    printf("COULD NOT SET MAC ADDRESS");
+    close(sockfd);
+    return 1;
+  }
+#endif /* ifdef __linux__*/
+
+#ifdef __APPLE__
+  assert(0 && "NOT IMPLEMENTED YET ON MACOS");
+#endif /* ifdef _APPLE__ */
+
+#ifdef __WIN32
+  assert(0 && "NOT IMPLEMENTED YET ON WINDOWS");
+#endif /* ifdef __WIN32__*/
+
 
   close(sockfd);
   return 0;
