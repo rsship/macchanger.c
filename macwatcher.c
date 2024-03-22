3a8737942c8a14dea20ec21b1922f73ab509812e
darth_vader <bozkayasalih01x@gmail.com>
2024-03-22 06:19:12 +0300

    add mac randomizer based on IMPC Protocol hearthbeat

--- macwatcher.c
+++ macwatcher.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <linux/errqueue.h>
 #include <net/if.h>
+#include <net/if_arp.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/ip_icmp.h>
@@ -12,11 +13,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
 
 #define PACKET_SIZE 64
 #define DEFAULT_HOST "google.com"
+#define SENDER_LIMIT 10
 
 #define free_defer(value)                                                      \
   do {                                                                         \
@@ -28,16 +31,17 @@ typedef struct {
   int sock;
   struct sockaddr_in to;
   struct sockaddr_in from;
-  int sender_count;
+  char *interface_name;
 } Echo;
+
+int sender_count;
 
 typedef struct {
   struct icmphdr hdr;
   char msg[PACKET_SIZE - sizeof(struct icmphdr)];
 } Ping_Packet;
 
-void usage() 
-{
+void usage() {
   printf("Net Watcher\n");
   printf("    Usage: netwatcher <INTERFACE_NAME> OPTIONS \n");
   printf("           Options:\n");
@@ -45,8 +49,7 @@ void usage()
          "default one is google.com");
 }
 
-char *shift_args(int *argc, char ***argv) 
-{
+char *shift_args(int *argc, char ***argv) {
   assert(*argc > 0);
   char *result = **argv;
   *argc -= 1;
@@ -54,14 +57,13 @@ char *shift_args(int *argc, char ***argv)
   return result;
 }
 
-u_char *get_mac_linux(int sock, const char *ifname) 
-{
+u_char *get_mac_linux(int sock, const char *ifname) {
   struct ifreq ifr;
   memset(&ifr, 0, sizeof(ifr));
   strcpy(ifr.ifr_name, ifname);
   int ret = ioctl(sock, SIOCGIFHWADDR, &ifr);
   if (ret < 0) {
-    printf("could not sent ioctl\n");
+    perror("could not get mac address");
     close(sock);
     exit(1);
   }
@@ -69,47 +71,45 @@ u_char *get_mac_linux(int sock, const char *ifname)
   return mac;
 }
 
-int set_mac_linux(int sockfd, const char *interface_name) 
-{
+int set_mac_linux(int sockfd, const char *interface_name, u_char mac[]) {
   struct ifreq ifr;
   // TURN DOWN INTERFACE;
-  printf("TURNING DOWN WI-FI INTERFACE\n");
   strncpy(ifr.ifr_name, interface_name, IFNAMSIZ);
-  if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0)
-    ifr.ifr_flags &= ~IFF_UP;
+  if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
+    perror("could not turn down");
+    close(sockfd);
+    return -1;
+  }
+
+  ifr.ifr_flags &= ~IFF_UP;
   if (ioctl(sockfd, SIOCSIFFLAGS, &ifr) < 0) {
-    printf("could not set flags dowl %s\n", interface_name);
+    perror("could not set flags down");
     close(sockfd);
-    return 1;
+    return -1;
   }
 
-  ifr.ifr_hwaddr.sa_family = AF_INET;
-  unsigned char new_mac[] = {0x00, 0x16, 0x3E, 0x33, 0x44, 0x66};
-
-  for (int i = 3; i < 6; ++i) {
-    new_mac[i] = rand() % 256;
-  }
-
-  memcpy(ifr.ifr_hwaddr.sa_data, new_mac, 6);
+  ifr.ifr_hwaddr.sa_family = ARPHRD_ETHER;
+  memcpy(ifr.ifr_hwaddr.sa_data, mac, 6);
   if (ioctl(sockfd, SIOCSIFHWADDR, &ifr) < 0) {
-    printf("could not set new mac %s\n", interface_name);
+    perror("could not set new mac");
     close(sockfd);
-    return 1;
+    return -1;
+  }
+  printf("MAC ADDRESS SET DONE\n");
+  // TURN UP INTERFACE AGAIN
+  if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0) {
+    perror("could not back up");
+    close(sockfd);
+    return -1;
   }
 
-  printf("SETTED MAC ADDRESS\n");
-  printf("%02X:%02X:%02X:%02X:%02X:%02X\n", new_mac[0], new_mac[1], new_mac[2],
-         new_mac[3], new_mac[4], new_mac[5]);
-
-  // TURN UP INTERFACE AGAIN
-  printf("TURNING UP WI-FI INTERFACE\n");
   ifr.ifr_flags |= IFF_UP;
   if (ioctl(sockfd, SIOCSIFFLAGS, &ifr) < 0) {
-    printf("could not set up flags %s\n", interface_name);
+    perror("could not set up flags");
     close(sockfd);
-    return 1;
+    return -1;
   }
-
+  close(sockfd);
   return 0;
 }
 
@@ -118,9 +118,29 @@ typedef struct {
   int thread_num;
 } Thread_Info;
 
-void *main_loop(void *icmp_raw) 
-{
+// RFC 1071
+uint16_t checksum(uint16_t *addr, int len) {
+  int count = len;
+  register uint32_t sum = 0;
+  uint16_t answer = 0;
+  while (count > 1) {
+    sum += *(addr++);
+    count -= 2;
+  }
 
+  if (count > 0) {
+    sum += *(uint8_t *)addr;
+  }
+
+  while (sum >> 16) {
+    sum = (sum & 0xffff) + (sum >> 16);
+  }
+
+  answer = ~sum;
+  return (answer);
+}
+
+void *main_loop(void *icmp_raw) {
   Echo echo = *(Echo *)icmp_raw;
   char packet[PACKET_SIZE];
   memset(&packet, 0, PACKET_SIZE);
@@ -130,38 +150,62 @@ void *main_loop(void *icmp_raw)
   ping_pkt->hdr.code = 0;
   ping_pkt->hdr.un.echo.sequence = htons(1);
   ping_pkt->hdr.un.echo.id = htons(getpid());
+  ping_pkt->hdr.checksum = checksum((uint16_t *)&packet, PACKET_SIZE);
 
-  while (1) {
+  int udp_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  srand(time(NULL));
+  u_char mac[] = {0x00, 0x16, 0x3E, 0x33, 0x44, 0x66};
+  for (int i = 2; i < 6; ++i) {
+    mac[i] = rand() % 256;
+  }
+
+  bool flag = 1;
+  while (flag) {
     sleep(1);
+
     if (sendto(echo.sock, packet, PACKET_SIZE, 0, (struct sockaddr *)&echo.to,
                sizeof(echo.to)) < 0) {
       perror("sendto:");
-      printf("errno %d\n", errno);
     }
     // Type
     //    8 for echo message;
     //    0 for echo reply message.
-
-    printf("Send Echo message\n");
-    echo.sender_count += 1;
+    Ping_Packet *ping_pkt = (Ping_Packet *)packet;
+    if (ping_pkt->hdr.type == 8) {
+      printf("sending ICMP MSG\n");
+      sender_count += 1;
+    }
+    if (sender_count > SENDER_LIMIT) {
+      /// DESTINATION UNREACHABLE
+      /// that's maybed caused by no doing mac randomization
+      /// so let's randomize the mac then
+      if (get_mac_linux(udp_sock, echo.interface_name) == NULL) {
+        printf("COULD NOT GET MAC ADDRESS");
+      }
+      if (set_mac_linux(udp_sock, echo.interface_name, mac) < 0) {
+        printf("COULD NOT SET MAC ADDRESS");
+      }
+      sender_count = 0;
+    }
   }
-  return NULL;
+  close(udp_sock);
 }
 
-int main(int argc, char **argv) 
-{
+int main(int argc, char **argv) {
   if (argc < 2) {
     usage();
     exit(1);
   }
   const char *program = shift_args(&argc, &argv);
-  const char *interface_name = shift_args(&argc, &argv);
+  char *interface_name = shift_args(&argc, &argv);
   const char *optional_host = shift_args(&argc, &argv);
   optional_host = (!optional_host) ? DEFAULT_HOST : optional_host;
 
   printf("RUNNING %s\n", program);
 
   Echo echo = {0};
+  echo.interface_name = interface_name;
   int result = 0;
 
 #ifdef __linux__
@@ -170,7 +214,11 @@ int main(int argc, char **argv)
     perror("socket:");
     free_defer(1);
   }
-  srand(time(NULL));
+  int ttl_val = 64;
+  if (setsockopt(echo.sock, SOL_IP, IP_TTL, &ttl_val, sizeof(ttl_val)) < 0) {
+    printf("Settting socket options to TLL failed\n");
+    free_defer(1);
+  }
 
   struct hostent *hp = gethostbyname(optional_host);
   if (hp == NULL) {
@@ -215,7 +263,6 @@ int main(int argc, char **argv)
     }
 
     Ping_Packet *ping_pkg = (Ping_Packet *)buffer;
-
     if (ping_pkg->hdr.type == 3) {
       switch (ping_pkg->hdr.code) {
       case 0:
@@ -239,7 +286,8 @@ int main(int argc, char **argv)
       }
     }
     if (ping_pkg->hdr.type == 0) {
-      printf("Received Echo Reply Msg\n");
+      printf("ICMP MSG RECEIVED\n");
+      sender_count = 0;
     }
   }
 
@@ -247,25 +295,6 @@ int main(int argc, char **argv)
     printf("could not join thread\n");
     free_defer(1);
   }
-  close(echo.sock);
-  return 0;
-
-  unsigned char *cur_mac = get_mac_linux(echo.sock, interface_name);
-  if (cur_mac == NULL) {
-    printf("COULD NOT GET MAC ADDRESS");
-    close(echo.sock);
-    return 1;
-  }
-  printf("CURRENT MAC ADDRESS\n");
-  printf("%02X:%02X:%02X:%02X:%02X:%02X\n", cur_mac[0], cur_mac[1], cur_mac[2],
-         cur_mac[3], cur_mac[4], cur_mac[5]);
-
-  if (set_mac_linux(echo.sock, interface_name) < 0) {
-    printf("COULD NOT SET MAC ADDRESS");
-    close(echo.sock);
-    return 1;
-  }
-
 #endif /* ifdef __linux__*/
 
 #ifdef __APPLE__
